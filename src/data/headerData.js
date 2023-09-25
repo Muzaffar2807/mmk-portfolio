93cdd81ab79ab50c2e789db25beffe8807057fb8
Muzaffar2807 <mmkhan2807@gmail.com>
2023-09-25 14:24:05 +0530

    resume update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -6,5 +6,6 @@ export const headerData = {
     "I'm Muzaffar Khan, a dedicated Fullstack developer with a strong focus on React, React Native, Node.js, Express.js technologies. With 2+ of hands-on experience, I've honed my skills in creating dynamic and responsive user interfaces.",
   //image: "https://i.ibb.co/ZmdNH6x/abu-said.jpg",
   //imagebw: "https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg",
-  resumePdf: "https://drive.google.com/file/d/10wYSVSKWs3WKjajSolfr-Mj5CxkV5t5r/view",
-}
+  resumePdf:
+    "https://drive.google.com/file/d/1pkt1B0r0TWXNSIjF8VMrTspEO-Gxk4c0/view",
+};
